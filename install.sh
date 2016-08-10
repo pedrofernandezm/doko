8df9fa22521ee7f3c0b3fe91f77c1eba0f6ff4c7
Pedro Fernández <pedrofernandezm@gmail.com>
2016-08-10 11:01:56 -0500

    Adding messaging to the installation

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -o /usr/local/bin/doko https://raw.githubusercontent.com/pedrofernandezm/doko/master/doko
+echo "Installing Doko..."
+curl -s -o /usr/local/bin/doko https://raw.githubusercontent.com/pedrofernandezm/doko/master/doko 
 chmod +x /usr/local/bin/doko
+echo "Doko installed correctly. Enjoy!"
